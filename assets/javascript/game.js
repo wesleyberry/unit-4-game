9360c8a3b0c9464a20f902e600a898b3f6d04364
Wesley Berry <wesleyberry52@gmail.com>
2019-03-06 18:00:45 -0600

    content submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,92 @@
 $(document).ready(function() {
     
-    //generate target score
-    startScore = 0;
-    function randomScore() {
-    min = Math.ceil(19);
-    max = Math.floor(120);
-    var targetScore = Math.floor(Math.random() * (max - min)) + min;
-    console.log(targetScore);
-    $("#targetScoreJQ").append(targetScore);
-    return targetScore;
-    }
-    var theRandomScore = randomScore();
+    //Declare and assign variables:
+    var startScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var generatedFirstButtonValue;
+    var generatedSecondButtonValue;
+    var generatedThirdButtonValue;
+    var generatedFourthButtonValue;
 
-    // generate values for each button
-    firstMin = Math.ceil(1);
-    firstMax = Math.floor(12);
-    var generatedFirstButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
-    var generatedSecondButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
-    var generatedThirdButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
-    var generatedFourthButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
-
-    console.log(startScore);
-    console.log(theRandomScore);
-
-    if(startScore == theRandomScore) {
-        wins++;
-        console.log("Wins: " + wins);
-    } 
-    if (startScore > theRandomScore) {
-        losses++;
-        console.log("Losses: " + losses);
+    //Generates a random target score:
+    function targetScoreFunc() {
+        min = Math.ceil(19);
+        max = Math.floor(120);
+        var targetScore = Math.floor(Math.random() * (max - min)) + min;
+        console.log(targetScore);
+        $("#targetScoreJQ").append(targetScore);
+        return targetScore;
     }
 
+    var theRandomScore = targetScoreFunc();
 
+        firstMin = Math.ceil(1);
+        firstMax = Math.floor(12);
+        generatedFirstButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+        generatedSecondButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+        generatedThirdButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+        generatedFourthButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+
+    // console.log(theRandomScore);
+    function checkWin(){
+        if(startScore == theRandomScore) {
+            wins++;
+            console.log("Wins: " + wins);
+            $("#winsJQ").text(wins);
+            startScore = 0;
+            generatedFirstButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedSecondButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedThirdButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedFourthButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            theRandomScore = targetScoreFunc();
+            $("#targetScoreJQ").text(theRandomScore);
+        } 
+        else if (startScore > theRandomScore) {
+            losses++;
+            console.log("Losses: " + losses);
+            $("#lossesJQ").text(losses);
+            startScore = 0; 
+            generatedFirstButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedSecondButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedThirdButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            generatedFourthButtonValue = Math.floor(Math.random() * (firstMax - firstMin)) + firstMin;
+            theRandomScore = targetScoreFunc();
+            $("#targetScoreJQ").text(theRandomScore);
+        }
+    }
+
+    // Click Events:
     $(".firstButton").on("click", function() {
         $(this).toggleClass('active');
-        // var startScore = $("<div>");
-        // $("#score")[0].innerHTML = score;
+        checkWin();
         startScore = generatedFirstButtonValue + startScore;
-        $("#yourScoreJQ").innerHTML = startScore;
+        $("#yourScoreJQ").text(startScore);
         console.log(startScore);
     });
 
     $(".secondButton").on("click", function() {
         $(this).toggleClass('active');
+        checkWin();
         startScore = generatedSecondButtonValue + startScore;
-        startScore = $("<div>");
+        $("#yourScoreJQ").text(startScore);
         console.log(startScore);
     });
 
     $(".thirdButton").on("click", function() {
         $(this).toggleClass('active');
+        checkWin();
         startScore = generatedThirdButtonValue + startScore;
-        $("#yourScoreJQ").append(startScore);
+        $("#yourScoreJQ").text(startScore);
         console.log(startScore);
     });
 
     $(".fourthButton").on("click", function() {
         $(this).toggleClass('active');
+        checkWin();
         startScore = generatedFourthButtonValue + startScore;
-        $("#yourScoreJQ").toggle(startScore);
+        $("#yourScoreJQ").text(startScore);
         console.log(startScore);
     });
-
-    //keep track of wins and losses
-
-    //reset
 
 });
